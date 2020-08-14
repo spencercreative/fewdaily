c99cd2d67f11c5780e76521458417b10cd3a40ca
Morgan Spencer <morgan@spencercreative.co>
2020-08-14 11:28:20 -0400

    Format post page

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 import ErrorPage from 'next/error'
 import MainLayout from 'layouts/main'
 import Sponsor from 'components/Sponsor'
 import Head from 'next/head'
 import { getPostBySlug, getAllPosts } from 'lib/api'
 import markdownToHtml from 'lib/markdownToHtml'
+import { theNamedDay, stringToSlug, splitSections } from 'lib/helpers'
 
 export default function Post({post}) {
     const router = useRouter()
@@ -18,18 +20,34 @@ export default function Post({post}) {
             {router.isFallback ? (
                 <div>Loading...</div>
             ) : (
-                <MainLayout day={post.day}>
-                    <article>
+                <MainLayout day={theNamedDay([post.slug])}>
+                    <article className="max-w-3xl mx-auto">
+                        {console.log(splitSections(post.content))}
                         <Head>
                             <title>
                                 {post.title} {post.slug} | Front-End Web Daily
                             </title>
                         </Head>
-                        <p className="mb-0 font-bold text-lg">{post.slug}</p>
-                        <h1 className="leading-tight uppercase">{post.title}</h1>
-                        <div className="mt-20" dangerouslySetInnerHTML={{__html: post.content}} />
+                        <header>
+                            <p className="mb-0 font-bold text-lg">{post.slug}</p>
+                            <h1 className="my-0 uppercase">{post.title}</h1>
+                            {post.tags !== undefined &&
+                                <p>
+                                    {post.tags.map((tag) => 
+                                        <Link href={'/tags/' + stringToSlug(tag)} key={stringToSlug(tag)}><a className="mr-3 inline-block">{tag}</a></Link>
+                                    )}
+                                </p>
+                            }
+                        </header>
+                        <Sponsor day={theNamedDay([post.slug])}/>
+                        <div className="mt-16" dangerouslySetInnerHTML={{__html: post.content}} />
+                        <footer>
+                            <div></div>
+                            <div>
+                                <button></button>
+                            </div>
+                        </footer>
                     </article>
-                    <Sponsor day={post.day}/>
                 </MainLayout>
             )}
         </>
@@ -55,7 +73,6 @@ export async function getStaticProps({ params }) {
     const post = getPostBySlug(params.slug, [
         'title',
         'slug',
-        'day',
         'content',
         'tags',
     ])

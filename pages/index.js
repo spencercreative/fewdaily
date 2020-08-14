4339b40413f09493725ce5d4e73e6eaf4c60325b
Morgan Spencer <morgan@spencercreative.co>
2020-08-14 17:49:40 -0400

    Add head to index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import MainLayout from 'layouts/MainLayout'
+import Head from 'next/head'
 import { getCurrentDay } from 'lib/helpers'
 import { getAllPosts } from 'lib/api'
 import PostList from 'components/PostList'
+import Sponsor from 'components/Sponsor'
 
 export default function Home({ allPosts }) {
 
     return (
         <MainLayout day={getCurrentDay()}>
+            <Head>
+                <title>Front-End Web Daily | News, tips and more delivered daily</title>
+            </Head>
+            <Sponsor/>
             {allPosts.length > 0 && <PostList posts={allPosts}/>}
         </MainLayout>
     )

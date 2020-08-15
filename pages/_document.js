3c43a06d52f945138cb31497d69ff87f4dceecfc
Morgan Spencer <morgan@spencercreative.co>
2020-08-15 18:11:37 -0400

    Add dynamic favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { getCurrentDay } from 'lib/helpers'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -7,14 +8,15 @@ class MyDocument extends Document {
   }
 
   render() {
+    var dayOfWeek = getCurrentDay()
     return (
       <Html lang="en" className="overflow-x-hidden w-full h-full">
         <Head>
           <meta name="theme-color" content="#042A2B"/>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-          <link rel="manifest" href="/site.webmanifest"/>
+          <link rel="apple-touch-icon" sizes="180x180" href={'/favicons/' + dayOfWeek + '/apple-touch-icon.png'}/>
+          <link rel="icon" type="image/png" sizes="32x32" href={'/favicons/' + dayOfWeek + '/favicon-32x32.png'}/>
+          <link rel="icon" type="image/png" sizes="16x16" href={'/favicons/' + dayOfWeek + '/favicon-16x16.png'}/>
+          <link rel="manifest" href={'/favicons/' + dayOfWeek + '/site.webmanifest'}/>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
         </Head>
         <body className="relative overflow-x-hidden w-full h-full">

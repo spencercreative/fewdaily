0c349c46c55d2bd74053c5ef43569a3b1995f17d
Morgan Spencer <morgan@spencercreative.co>
2020-08-20 17:08:14 -0400

    Update assets design and layout

--- pages/[slug]/assets.js
+++ pages/[slug]/assets.js
@@ -9,19 +9,19 @@ import markdownToHtml from 'lib/markdownToHtml'
 import { theNamedDay, splitSections, makeExcerpt, dayTitle, theDateString } from 'lib/helpers'
 
 function SocialImage(props) {
-    const cornerStyles = [ 'absolute', 'py-8', 'px-10', 'text-2xs' ]
+    const cornerStyles = 'absolute py-8 px-10 text-2xs whitespace-no-wrap font-black'
 
     return (
-        <div style={{maxWidth: props.width}} className="inline-block text-left">
+        <div style={{maxWidth: props.width}} className="asset-container inline-block text-left">
             <RenderAsImage>
-                <div className={'instagram p-10 flex font-bold text-sm relative' + ' bg-' + theNamedDay([props.post.slug])} style={{width: props.width, height: props.height}}>
-                    <span className={'top-0 left-0 uppercase' + ' ' + cornerStyles.join(' ')}>Front-End Web Daily</span>
-                    <span className={'top-0 right-0 uppercase' + ' ' + cornerStyles.join(' ')}>{theDateString(props.post.slug)}</span>
+                <div className={'instagram p-10 flex text-sm relative' + ' bg-' + theNamedDay([props.post.slug])} style={{width: props.width, height: props.height}}>
+                    <span className={'top-0 left-0 uppercase' + ' ' + cornerStyles}>Front-End Web Daily</span>
+                    <span className={'top-0 right-0 uppercase' + ' ' + cornerStyles}>{theDateString(props.post.slug)}</span>
                     <div className={props.className !== undefined && props.className}>
                         {props.children}
                     </div>
-                    <span className={'bottom-0 left-0 uppercase' + ' ' + cornerStyles.join(' ')}>More at fewdaily.com</span>
-                    <span className={'bottom-0 right-0' + ' ' + cornerStyles.join(' ')}>@fewdaily</span>
+                    <span className={'bottom-0 left-0 uppercase' + ' ' + cornerStyles}>More at fewdaily.com</span>
+                    <span className={'bottom-0 right-0' + ' ' + cornerStyles}>@fewdaily</span>
                 </div>
             </RenderAsImage>
         </div>
@@ -48,95 +48,107 @@ export default function Assets({post}) {
                             <p className="mb-0 font-bold text-sm"><Link href={'/' + post.slug}><a>{theDateString(post.slug)}</a></Link></p>
                             <h1 className="my-0 uppercase">Assets</h1>
                         </header>
+
+                        <section>
                         
-                        <h3 className="text-center">Square</h3>
+                            <h3 className="text-center">Square</h3>
 
-                        <div className="text-center">
-                            <SocialImage post={post} width="500px" height="500px">
-                                <h3 className="m-0 uppercase text-4xl my-6">{dayTitle(post.slug)}</h3>
-                                {makeExcerpt(post.content).split(', ').map((element, index) =>
-                                    <h4 className="text-xl mt-0 mb-3" key={index}>{element}</h4>
-                                )}
-                            </SocialImage>
-
-                            {splitSections(post.renderedContent).map((element, index) =>
-                                <SocialImage post={post} width="500px" height="500px" key={index}>
-                                    <div dangerouslySetInnerHTML={{__html: element}} />
+                            <div className="text-center">
+                                <SocialImage post={post} width="500px" height="500px">
+                                    <h3 className="m-0 uppercase text-4xl my-6">{dayTitle(post.slug)}</h3>
+                                    {makeExcerpt(post.content).split(', ').map((element, index) =>
+                                        <h4 className="text-xl mt-0 mb-3" key={index}>{element}</h4>
+                                    )}
                                 </SocialImage>
-                            )}
 
-                            <SocialImage post={post} width="500px" height="500px">
-                                <div className="h-full flex items-center">
-                                    <h3 className="m-0 uppercase text-4xl">Check Back Tomorrow</h3>
-                                </div>
-                            </SocialImage>
-                        </div>
-
-                        <div className="flex justify-center my-8">
-                            <button>Download Square Files</button>
-                        </div>
-
-                        <hr/>
-                        
-                        <h3 className="text-center">Tall</h3>
-
-                        <div className="text-center">
-                            <SocialImage post={post} width="360px" height="640px">
-                                <h3 className="m-0 uppercase text-3xl my-6">{dayTitle(post.slug)}</h3>
-                                {makeExcerpt(post.content).split(', ').map((element, index) =>
-                                    <h4 className="text-base mt-0 mb-3" key={index}>{element}</h4>
+                                {splitSections(post.renderedContent).map((element, index) =>
+                                    <SocialImage post={post} width="500px" height="500px" key={index}>
+                                        <div dangerouslySetInnerHTML={{__html: element}} />
+                                    </SocialImage>
                                 )}
-                            </SocialImage>
 
-                            {splitSections(post.renderedContent).map((element, index) =>
-                                <SocialImage post={post} width="360px" height="640px" key={index}>
-                                    <div dangerouslySetInnerHTML={{__html: element}} />
-                                </SocialImage>
-                            )}
-
-                            <SocialImage post={post} width="360px" height="640px">
-                                <div className="h-full flex items-center">
-                                    <h3 className="m-0 uppercase text-3xl">Check Back Tomorrow</h3>
-                                </div>
-                            </SocialImage>
-                        </div>
-
-                        <div className="flex justify-center my-8">
-                            <button>Download Tall Files</button>
-                        </div>
-
-                        <hr/>
-                        
-                        <h3 className="text-center">Wide</h3>
-
-                        <div className="text-center">
-                            <SocialImage post={post} width="640px" height="360px" className="flex-1">
-                                <div className="h-full flex items-center w-full gap-4">
-                                    <h3 className="m-0 uppercase text-3xl my-6 w-1/2">{dayTitle(post.slug)}</h3>
-                                    <div>
-                                        {makeExcerpt(post.content).split(', ').map((element, index) =>
-                                            <h4 className="text-base mt-0 mb-3" key={index}>{element}</h4>
-                                        )}
+                                <SocialImage post={post} width="500px" height="500px">
+                                    <div className="h-full flex items-center">
+                                        <h3 className="m-0 uppercase text-4xl">Check Back Tomorrow</h3>
                                     </div>
-                                </div>
-                            </SocialImage>
-
-                            {splitSections(post.renderedContent).map((element, index) =>
-                                <SocialImage className="flex" post={post} width="640px" height="360px" key={index}>
-                                    <div dangerouslySetInnerHTML={{__html: element}} />
                                 </SocialImage>
-                            )}
+                            </div>
 
-                            <SocialImage post={post} width="640px" height="360px" className="flex-1">
-                                <div className="h-full flex items-center w-full">
-                                    <h3 className="m-0 uppercase text-3xl w-full">Check Back Tomorrow</h3>
-                                </div>
-                            </SocialImage>
-                        </div>
+                            <div className="flex justify-center my-8">
+                                <button>Download Square Files</button>
+                            </div>
 
-                        <div className="flex justify-center my-8">
-                            <button>Download Wide Files</button>
-                        </div>
+                        </section>
+
+                        <hr/>
+
+                        <section>
+                        
+                            <h3 className="text-center">Tall</h3>
+
+                            <div className="text-center">
+                                <SocialImage post={post} width="360px" height="640px">
+                                    <h3 className="m-0 uppercase text-3xl my-6">{dayTitle(post.slug)}</h3>
+                                    {makeExcerpt(post.content).split(', ').map((element, index) =>
+                                        <h4 className="text-base mt-0 mb-3" key={index}>{element}</h4>
+                                    )}
+                                </SocialImage>
+
+                                {splitSections(post.renderedContent).map((element, index) =>
+                                    <SocialImage post={post} width="360px" height="640px" key={index}>
+                                        <div dangerouslySetInnerHTML={{__html: element}} />
+                                    </SocialImage>
+                                )}
+
+                                <SocialImage post={post} width="360px" height="640px">
+                                    <div className="h-full flex items-center">
+                                        <h3 className="m-0 uppercase text-3xl">Check Back Tomorrow</h3>
+                                    </div>
+                                </SocialImage>
+                            </div>
+
+                            <div className="flex justify-center my-8">
+                                <button>Download Tall Files</button>
+                            </div>
+
+                        </section>
+
+                        <hr/>
+
+                        <section>
+                        
+                            <h3 className="text-center">Wide</h3>
+
+                            <div className="text-center">
+                                <SocialImage post={post} width="640px" height="360px" className="flex-1">
+                                    <div className="h-full flex items-center w-full gap-10">
+                                        <h3 className="m-0 uppercase text-3xl my-6 w-1/2 text-right">{dayTitle(post.slug)}</h3>
+                                        <div className="w-1/2">
+                                            {makeExcerpt(post.content).split(', ').map((element, index) =>
+                                                <h4 className="text-base mt-0 mb-3" key={index}>{element}</h4>
+                                            )}
+                                        </div>
+                                    </div>
+                                </SocialImage>
+
+                                {splitSections(post.renderedContent).map((element, index) =>
+                                    <SocialImage className="flex" post={post} width="640px" height="360px" key={index}>
+                                        <div dangerouslySetInnerHTML={{__html: element}} />
+                                    </SocialImage>
+                                )}
+
+                                <SocialImage post={post} width="640px" height="360px" className="flex-1">
+                                    <div className="h-full flex items-center w-full">
+                                        <h3 className="m-0 uppercase text-3xl w-full text-center">Check Back Tomorrow</h3>
+                                    </div>
+                                </SocialImage>
+                            </div>
+
+                            <div className="flex justify-center my-8">
+                                <button>Download Wide Files</button>
+                            </div>
+
+                        </section>
 
                     </article>
                 </MainLayout>

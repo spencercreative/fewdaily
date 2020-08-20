f74e054f777178f59c088fb8a41684847ed2f71c
Morgan Spencer <morgan@spencercreative.co>
2020-08-20 19:42:11 -0400

    Update metadata head

--- components/MetaHead.js
+++ components/MetaHead.js
@@ -28,25 +28,25 @@ function HeadMetadata({title, description, day, type}) {
             
             <title>{metaTitle}</title>
             <meta name="title" content={metaTitle} />
-            <meta property="og:title" content={metaTitle} />
-            <meta property="twitter:title" content={metaTitle} />
+            <meta name="og:title" content={metaTitle} />
+            <meta name="twitter:title" content={metaTitle} />
 
             {description !== undefined &&
             <>
                 <meta name="description" content={description} />
-                <meta property="og:description" content={description} />
-                <meta property="twitter:description" content={description} />
+                <meta name="og:description" content={description} />
+                <meta name="twitter:description" content={description} />
             </>
             }
 
-            <meta property="og:url" content={url} />
-            <meta property="twitter:url" content={url} />
+            <meta name="og:url" content={url} />
+            <meta name="twitter:url" content={url} />
 
             {day !== undefined ?
             <>
-            <meta property="og:image" content={hostname + '/social-cards/' + day + '.png'} />
-            <meta property="og:image:secure_url" content={hostname + '/social-cards/' + day + '.png'} />
-            <meta property="twitter:image" content={hostname + '/social-cards/' + day + '.png'} />
+            <meta name="og:image" content={hostname + '/social-cards/' + day + '.png'} />
+            <meta name="og:image:secure_url" content={hostname + '/social-cards/' + day + '.png'} />
+            <meta name="twitter:image" content={hostname + '/social-cards/' + day + '.png'} />
             <link rel="apple-touch-icon" sizes="180x180" href={'/favicons/' + day + '/apple-touch-icon.png'}/>
             <link rel="icon" type="image/png" sizes="32x32" href={'/favicons/' + day + '/favicon-32x32.png'}/>
             <link rel="icon" type="image/png" sizes="16x16" href={'/favicons/' + day + '/favicon-16x16.png'}/>
@@ -54,9 +54,9 @@ function HeadMetadata({title, description, day, type}) {
             </>
             :
             <>
-            <meta property="og:image" content={hostname + '/social-cards/monday.png'} />
-            <meta property="og:image:secure_url" content={hostname + '/social-cards/' + day + '.png'} />
-            <meta property="twitter:image" content={hostname + '/social-cards/monday.png'} />
+            <meta name="og:image" content={hostname + '/social-cards/monday.png'} />
+            <meta name="og:image:secure_url" content={hostname + '/social-cards/' + day + '.png'} />
+            <meta name="twitter:image" content={hostname + '/social-cards/monday.png'} />
             <link rel="apple-touch-icon" sizes="180x180" href={'/favicons/monday/apple-touch-icon.png'}/>
             <link rel="icon" type="image/png" sizes="32x32" href={'/favicons/monday/favicon-32x32.png'}/>
             <link rel="icon" type="image/png" sizes="16x16" href={'/favicons/monday/favicon-16x16.png'}/>
@@ -64,17 +64,17 @@ function HeadMetadata({title, description, day, type}) {
             </>
             }
             
-            {type !== undefined ? <meta property="og:type" content={type} /> : <meta property="og:type" content="website" />}
+            {type !== undefined ? <meta name="og:type" content={type} /> : <meta name="og:type" content="website" />}
             
-            <meta property="og:locale" content="en_US" />
-            <meta property="og:image:type" content="image/png" />
-            <meta property="og:image:width" content="1920" />
-            <meta property="og:image:height" content="1080" />
-            <meta property="og:image:alt" content="Calendar on computer logo for Front-End Web Daily" />
-            <meta property="twitter:image:alt" content="Calendar on computer logo for Front-End Web Daily" />
+            <meta name="og:locale" content="en_US" />
+            <meta name="og:image:type" content="image/png" />
+            <meta name="og:image:width" content="1920" />
+            <meta name="og:image:height" content="1080" />
+            <meta name="og:image:alt" content="Calendar on computer logo for Front-End Web Daily" />
+            <meta name="twitter:image:alt" content="Calendar on computer logo for Front-End Web Daily" />
             <meta name="twitter:creator" content="@fewdaily"/>
             <meta name="twitter:site" content="@fewdaily"/>
-            <meta property="twitter:card" content="summary_large_image" />
+            <meta name="twitter:card" content="summary_large_image" />
         </Head>
     )
 }

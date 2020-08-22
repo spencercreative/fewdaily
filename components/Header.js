ccfd2c60b6416d50ce00a3a7250423d2485a0638
Morgan Spencer <morgan@spencercreative.co>
2020-08-22 08:53:36 -0400

    Update header nav

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import { withRouter } from 'next/router'
 import Link from 'next/link'
-import { FiTag, FiInstagram, FiFacebook, FiTwitter, FiYoutube, FiLinkedin, FiMic } from "react-icons/fi"
+import { FiTag, FiInstagram, FiFacebook, FiTwitter, FiYoutube, FiLinkedin, FiMic, FiCornerUpLeft } from "react-icons/fi"
 
 const SocialLink = (props) => (
     <li className="p-4">
@@ -27,6 +27,7 @@ function SocialToggle() {
         main.classList.add('hidden')
         footer.classList.add('hidden')
         headerHome.setAttribute('tabindex', -1)
+        button.innerHTML = '<span className="block">Close</span>'
     } else {
         menu.classList.add('hidden')
         menu.classList.remove('flex', 'active')
@@ -35,6 +36,7 @@ function SocialToggle() {
         main.classList.remove('hidden')
         footer.classList.remove('hidden')
         headerHome.removeAttribute('tabindex')
+        button.innerHTML = '@<span class="hidden md:block">fewdaily</span>'
     }
 }
 
@@ -45,7 +47,7 @@ function Header({day, router}) {
 
             {router.pathname === '/tags' ? (
                 <a className="flex items-center leading-none text-lg font-bold" title="Tags" onClick={() => router.back()}>
-                    <FiTag aria-hidden="true"/><span className="ml-1 hidden md:block">Tags</span>
+                    <FiCornerUpLeft aria-hidden="true"/><span className="ml-1 hidden md:block">Back</span>
                 </a>
             ) : (
                 <>
@@ -54,27 +56,28 @@ function Header({day, router}) {
                         <FiTag aria-hidden="true"/><span className="ml-1 hidden md:block">Tags</span>
                     </a>
                 </Link>
-
-                <div id="header-home" className="flex-1 px-4 text-center uppercase md:text-2xl">
-                    <Link href="/"><a className="leading-none">Front-End Web Daily</a></Link>
-                </div>
-
-                <button id="social-toggle" title="Social Media" className="flex items-center leading-none text-lg font-bold" onClick={SocialToggle}>
-                    @<span className="hidden md:block">fewdaily</span>
-                </button>
-
-                <nav id="social-nav" className="absolute hidden items-center justify-center translate-x-full top-0 left-0 w-full h-full py-20 px-wrap bg-white z-10 overflow-y-auto transition-transform duration-500">
-                    <ul className="flex flex-wrap justify-center items-center text-6xl -m-4">
-                        <SocialLink icon={<FiInstagram className="stroke-1-1/2"/>} to="https://www.instagram.com/fewdaily/" title="Instagram" />
-                        <SocialLink icon={<FiTwitter className="stroke-1-1/2"/>} to="https://twitter.com/fewdaily" title="Twitter" />
-                        <SocialLink icon={<FiFacebook className="stroke-1-1/2"/>} to="https://www.facebook.com/fewdaily/" title="Facebook" />
-                        <SocialLink icon={<FiYoutube className="stroke-1-1/2"/>} to="https://www.youtube.com/channel/UC4IfUmMAi_-vT4ELn4GgVLQ" title="YouTube" />
-                        <SocialLink icon={<FiLinkedin className="stroke-1-1/2"/>} to="https://www.linkedin.com/company/fewdaily/" title="LinkedIn" />
-                        <SocialLink icon={<FiMic className="stroke-1-1/2"/>} to="https://anchor.fm/fewdaily" title="Podcast on Anchor.fm" />
-                    </ul>
-                </nav>
                 </>
             )}
+
+            <div id="header-home" className="flex-1 px-4 text-center uppercase md:text-2xl">
+                <Link href="/"><a className="leading-none">Front-End Web Daily</a></Link>
+            </div>
+
+            <button id="social-toggle" title="Social Media" className="flex items-center leading-none text-lg font-bold" onClick={SocialToggle}>
+                @<span className="hidden md:block">fewdaily</span>
+            </button>
+
+            <nav id="social-nav" className="fixed hidden items-start md:items-center justify-center translate-x-full top-0 left-0 w-full h-full py-20 px-wrap bg-white z-10 overflow-y-auto">
+                <ul className="flex flex-wrap justify-center items-center text-6xl -m-4">
+                    <SocialLink icon={<FiInstagram className="stroke-1-1/2"/>} to="https://www.instagram.com/fewdaily/" title="Instagram" />
+                    <SocialLink icon={<FiTwitter className="stroke-1-1/2"/>} to="https://twitter.com/fewdaily" title="Twitter" />
+                    <SocialLink icon={<FiFacebook className="stroke-1-1/2"/>} to="https://www.facebook.com/fewdaily/" title="Facebook" />
+                    <SocialLink icon={<FiYoutube className="stroke-1-1/2"/>} to="https://www.youtube.com/channel/UC4IfUmMAi_-vT4ELn4GgVLQ" title="YouTube" />
+                    <SocialLink icon={<FiLinkedin className="stroke-1-1/2"/>} to="https://www.linkedin.com/company/fewdaily/" title="LinkedIn" />
+                    <SocialLink icon={<FiMic className="stroke-1-1/2"/>} to="https://anchor.fm/fewdaily" title="Podcast on Anchor.fm" />
+                </ul>
+            </nav>
+
         </header>
     )
 }

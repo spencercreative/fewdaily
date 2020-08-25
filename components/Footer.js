9461e455a12b3f19d0ea002f8cebc9124281e81e
Morgan Spencer <morgan@spencercreative.co>
2020-08-25 10:47:41 -0400

    Add Submit a Story and Sponsorships

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Footer = (props) => (
     <footer id="footer" role="contentinfo" className={'px-wrap text-center py-2' + ' ' + props.day}>
-        <p className="text-xs mb-0">Copyright &copy; { new Date().getFullYear() } <Link href="/"><a>Front-End Web Daily</a></Link>. All rights reserved.</p>
+        <p className="text-xs mb-0">Copyright &copy; { new Date().getFullYear() } <Link href="/"><a>Front-End Web Daily</a></Link>. All rights reserved. | <Link href="/submit"><a>Submit a Story</a></Link> | <Link href="/sponsorships"><a>Promote Your Business</a></Link></p>
     </footer>
 )
 

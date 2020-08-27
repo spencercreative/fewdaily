8368609c740769266c8bd911350e103483437096
Morgan Spencer <morgan@spencercreative.co>
2020-08-26 23:46:05 -0400

    Add RSS feed

--- next.config.js
+++ next.config.js
@@ -4,5 +4,12 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
     pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-    target: 'serverless'
+    target: 'serverless',
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+          require('./lib/rss');
+        }
+  
+        return config;
+    },
 })

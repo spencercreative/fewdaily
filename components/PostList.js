c97a71ec282256a985053e1e11edd05e84940ef1
Morgan Spencer <morgan@spencercreative.co>
2020-08-28 16:50:17 -0400

    Modified how sponsors are added

--- components/PostList.js
+++ components/PostList.js
@@ -1,33 +1,105 @@
 import Link from 'next/link'
-import { theNamedDay, stringToSlug, makeExcerpt, dayTitle, listDate } from 'lib/helpers'
+import { theNamedDay, stringToSlug, makeExcerpt, dayTitle, listDate, getCurrentDay } from 'lib/helpers'
+import { sponsors } from 'lib/sponsors'
+
+function shuffleArray(array) {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array.slice(0,1);
+}
 
 export default function PostList( props ) {
+    
+    if ( props.featured !== undefined ) {
+        var featuredPosts = props.posts.slice(0,props.featured)
+        var posts = props.posts.slice(props.featured)
+    } else {
+        var posts = props.posts
+    }
+
     return (
         <>
         <p className="mt-0 mb-1 text-xs text-right">{props.count} post{ props.count > 1 && 's' }</p>
-        <ul className="border-t border-solid border-gray">
-            {props.posts.map((post) =>
-                <li className={'post-list-item flex flex-col md:flex-row justify-between md:items-center text-xl py-3 border-b border-solid border-gray' + ' ' + theNamedDay(post.slug)} key={post.slug}>
-                    <div className="mb-4 md:mr-4 md:mb-0">
-                        <p className="mb-0 text-xs font-bold">
-                            {listDate(post.slug)}
-                        </p>
-                        <Link href={'/' + post.slug}><a className="font-black text-2xl">{dayTitle(post.slug)}</a></Link>
-                        { theNamedDay(post.slug) !== 'tuesday' ? (
-                            <p className="mb-0 text-base">{makeExcerpt(post.content)}</p>
-                        ) : (
-                            <p className="mb-0 text-base">{post.excerpt}</p>
-                        )
-                        }
-                    </div>
+        <ul id="postList" className="border-t border-solid border-gray">
+            { props.featured !== undefined && featuredPosts.map((post) =>
+                <li className={'flex flex-col items-center featured post-list-item text-center text-xl py-8 border-b border-solid border-gray' + ' ' + theNamedDay(post.slug)} key={post.slug}>
+                    <p className="mb-0 text-base font-bold">
+                        {listDate(post.slug)}
+                    </p>
+                    <Link href={'/' + post.slug}><a className="font-black text-4xl">{dayTitle(post.slug)}</a></Link>
+                    <p className="my-4 text-xl max-w-3xl mx-auto">
+                        { theNamedDay(post.slug) !== 'tuesday' ? makeExcerpt(post.content) : post.excerpt }
+                    </p>
                     {post.tags !== undefined &&
-                        <div className="flex flex-wrap md:justify-end md:max-w-xs">
+                        <div className="flex flex-wrap justify-center">
                             {post.tags.map((tag) => 
                                 <Link href={'/tags/' + stringToSlug(tag)} key={stringToSlug(tag)}><a className="mr-3 md:ml-3 md:mr-0 text-lg md:text-base">{tag}</a></Link>
                             )}
                         </div>
                     }
                 </li>
+            )}
+            {posts.map((post, i) =>
+                ( typeof sponsors != "undefined" && sponsors != null && sponsors.length != null
+                && sponsors.length > 0 && (i + 1) % 7 === 0) ? (
+                    <>
+                    {shuffleArray(sponsors).map((sponsor, i) => 
+                        <li className={'post-list-item flex flex-col md:flex-row justify-between md:items-center text-xl py-3 border-b border-solid border-gray' + ' ' + getCurrentDay()} key={i}>
+                            <div className="mb-4 md:mr-4 md:mb-0">
+                                <p className="mb-0 text-xs font-bold">Sponsored</p>
+                                <a className="font-black text-2xl" href="https://spencercreative.co" target="_blank">
+                                    {sponsor.name}
+                                </a>
+                                <p className="mb-0 text-base">
+                                {sponsor.message}
+                                </p>
+                            </div>
+                        </li>
+                    )}
+                    <li className={'post-list-item flex flex-col md:flex-row justify-between md:items-center text-xl py-3 border-b border-solid border-gray' + ' ' + theNamedDay(post.slug)} key={post.slug}>
+                        <div className="mb-4 md:mr-4 md:mb-0">
+                            <p className="mb-0 text-xs font-bold">
+                                {listDate(post.slug)}
+                            </p>
+                            <Link href={'/' + post.slug}><a className="font-black text-2xl">{dayTitle(post.slug)}</a></Link>
+                            <p className="mb-0 text-base">
+                                { theNamedDay(post.slug) !== 'tuesday' ? makeExcerpt(post.content) : post.excerpt }
+                            </p>
+                        </div>
+                        {post.tags !== undefined &&
+                            <div className="flex flex-wrap md:justify-end md:max-w-xs">
+                                {post.tags.map((tag) => 
+                                    <Link href={'/tags/' + stringToSlug(tag)} key={stringToSlug(tag)}><a className="mr-3 md:ml-3 md:mr-0 text-lg md:text-base">{tag}</a></Link>
+                                )}
+                            </div>
+                        }
+                    </li>
+                    </>
+                ) : (
+                    <li className={'post-list-item flex flex-col md:flex-row justify-between md:items-center text-xl py-3 border-b border-solid border-gray' + ' ' + theNamedDay(post.slug)} key={post.slug}>
+                        <div className="mb-4 md:mr-4 md:mb-0">
+                            <p className="mb-0 text-xs font-bold">
+                                {listDate(post.slug)}
+                            </p>
+                            <Link href={'/' + post.slug}><a className="font-black text-2xl">{dayTitle(post.slug)}</a></Link>
+                            <p className="mb-0 text-base">
+                                { theNamedDay(post.slug) !== 'tuesday' ? makeExcerpt(post.content) : post.excerpt }
+                            </p>
+                        </div>
+                        {post.tags !== undefined &&
+                            <div className="flex flex-wrap md:justify-end md:max-w-xs">
+                                {post.tags.map((tag) => 
+                                    <Link href={'/tags/' + stringToSlug(tag)} key={stringToSlug(tag)}><a className="mr-3 md:ml-3 md:mr-0 text-lg md:text-base">{tag}</a></Link>
+                                )}
+                            </div>
+                        }
+                    </li>
+                )
             )}
         </ul>
         </>
--- components/Sponsor.js
+++ components/Sponsor.js
@@ -1,103 +1,25 @@
 import Link from 'next/link'
-import { getCurrentDay } from 'lib/helpers'
-
-const Wrapper = (props) => (
-    <aside role="complementary" className={'sponsor px-4 py-8 mb-10 rounded-md text-center relative' + ' bg-' + props.day}>
-        <div className="text-xl font-black leading-none">{props.children}</div>
-        <p className="text-xs text-right mb-0 absolute bottom-0 right-0 py-1 px-2"><Link href="/sponsorships"><a>Learn more about our sponsorships</a></Link></p>
-    </aside>
-)
-
-const Monday = () => (
-    <>This is a <a>Monday</a> sponsor.</>
-)
-
-const Tuesday = () => (
-    <>This is a <a>Tuesday</a> sponsor.</>
-)
-
-const Wednesday = () => (
-    <>This is a <a>Wednesday</a> sponsor.</>
-)
-
-const Thursday = () => (
-    <>This is a <a>Thursday</a> sponsor.</>
-)
-
-const Friday = () => (
-    <>This is a <a>Friday</a> sponsor.</>
-)
-
-const Saturday = () => (
-    <>This is a <a>Saturday</a> sponsor.</>
-)
-
-const Sunday = () => (
-    <>This is a <a>Sunday</a> sponsor.</>
-)
-
-const General = () => (
-    <>This is a <a>general</a> sponsor.</>
-)
+import { sponsors } from 'lib/sponsors'
 
 export default function Sponsor(props) {
 
-    if ( props.day === 'monday' ) {
+    if ( typeof sponsors != "undefined" && sponsors != null && sponsors.length != null
+    && sponsors.length ) {
         return (
-            null
-            // <Wrapper day={props.day}>
-            //     <Monday/>
-            // </Wrapper> 
-        )
-    } else if ( props.day === 'tuesday' ) {
-        return (
-            null
-            // <Wrapper day={props.day}>
-            //     <Tuesday/>
-            // </Wrapper> 
-        )
-    } else if ( props.day === 'wednesday' ) {
-        return (
-            null
-            // <Wrapper day={props.day}>
-            //     <Wednesday/>
-            // </Wrapper> 
-        )
-    } else if ( props.day === 'thursday' ) {
-        return (
-            null
-            // <Wrapper day={props.day}>
-            //     <Thursday/>
-            // </Wrapper> 
-        )
-    } else if ( props.day === 'friday' ) {
-        return (
-            null
-            // <Wrapper day={props.day}>
-            //     <Friday/>
-            // </Wrapper> 
-        )
-    } else if ( props.day === 'saturday' ) {
-        return (
-            null
-            // <Wrapper day={props.day}>
-            //     <Saturday/>
-            // </Wrapper> 
-        )
-    } else if ( props.day === 'sunday' ) {
-        return (
-            null
-            // <Wrapper day={props.day}>
-            //     <Sunday/>
-            // </Wrapper> 
+            <>
+            {sponsors.map((sponsor, i) => sponsor.day === props.day &&
+                <aside role="complementary" className={'sponsor px-4 py-8 mb-10 rounded-md text-center relative' + ' bg-' + props.day} key={i}>
+                    <div className="font-black leading-none">
+                        <p className="text-xl mb-3">Our <span className="capitalize">{props.day}</span> posts are sponsored by <a href={sponsor.link} target="_blank">{sponsor.name}</a>.</p>
+                        <p className="mb-0 font-normal text-base">{sponsor.message}</p>
+                    </div>
+                    <p className="text-xs text-right mb-0 absolute bottom-0 right-0 py-1 px-2"><Link href="/sponsorships"><a>Learn more about our sponsorships</a></Link></p>
+                </aside>
+            )}
+            </>
         )
     } else {
-        return (
-            null
-            // <Wrapper day={getCurrentDay()}>
-            //     <General/>
-            // </Wrapper> 
-        )
+        return null
     }
     
 }

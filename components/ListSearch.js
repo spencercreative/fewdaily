b96b2d6341e8a7ae6af3fc70fb092a2d2702ced3
Morgan Spencer <morgan@spencercreative.co>
2020-08-30 17:20:10 -0400

    Update list to allow for search query strings

--- components/ListSearch.js
+++ components/ListSearch.js
@@ -1,16 +1,16 @@
-import { listSearchSort, getSearchQuery } from 'lib/helpers'
+import { sortList, listSearchSort } from 'lib/helpers'
+import { useRouter } from "next/router";
 
 export default function ListSearch() {
+    const {query: { s }} = useRouter();
 
-    if (typeof window !== "undefined") {
-        // getSearchQuery()
-    }
+    sortList(s)
 
     return (
         <>
         <div className="mb-4 flex items-center flex-col md:flex-row print:hidden">
             <label className="my-0 mr-4 font-black text-base" htmlFor="list-search">Search:</label>
-            <input id="list-search" type="text" className="mx-0 flex-1 text-base" placeholder="Type to sort through posts..." onChange={listSearchSort} />
+            <input id="list-search" type="text" className="mx-0 flex-1 text-base" placeholder="Type to sort through posts..." onChange={listSearchSort} defaultValue={s} autoComplete="off" />
         </div>
         </>
     )

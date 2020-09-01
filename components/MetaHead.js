20d5e16763a7c754155b5dcd283e9c4e43eb867e
Morgan Spencer <morgan@spencercreative.co>
2020-09-01 11:40:44 -0400

    Add syntax highlighting to code blocks

--- components/MetaHead.js
+++ components/MetaHead.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Head from 'next/head'
 
-function HeadMetadata({title, description, day, type}) {
+function HeadMetadata(props) {
 
-    if (title !== undefined) {
-        var metaTitle = title + ' | Front-End Web Daily'
+    if (props.title !== undefined) {
+        var metaTitle = props.title + ' | Front-End Web Daily'
     } else {
         var metaTitle = 'Front-End Web Daily | News, tips and more delivered daily'
     }
@@ -17,8 +17,8 @@ function HeadMetadata({title, description, day, type}) {
 
     const hostname = 'https://fewdaily.com'
 
-    if (description !== undefined) {
-        var description = description
+    if (props.description !== undefined) {
+        var description = props.description
     } else {
         var description = "Front-end news, tips, and more delivered daily across the web."
     }
@@ -42,20 +42,20 @@ function HeadMetadata({title, description, day, type}) {
             <meta property="og:url" content={url} />
             <meta name="twitter:url" content={url} />
 
-            {day !== undefined ?
+            {props.day !== undefined ?
             <>
-            <meta property="og:image" content={hostname + '/social-cards/' + day + '.png'} />
-            <meta property="og:image:secure_url" content={hostname + '/social-cards/' + day + '.png'} />
-            <meta name="twitter:image" content={hostname + '/social-cards/' + day + '.png'} />
-            <link rel="apple-touch-icon" sizes="180x180" href={'/favicons/' + day + '/apple-touch-icon.png'}/>
-            <link rel="icon" type="image/png" sizes="32x32" href={'/favicons/' + day + '/favicon-32x32.png'}/>
-            <link rel="icon" type="image/png" sizes="16x16" href={'/favicons/' + day + '/favicon-16x16.png'}/>
-            <link rel="manifest" href={'/favicons/' + day + '/site.webmanifest'}/>
+            <meta property="og:image" content={hostname + '/social-cards/' + props.day + '.png'} />
+            <meta property="og:image:secure_url" content={hostname + '/social-cards/' + props.day + '.png'} />
+            <meta name="twitter:image" content={hostname + '/social-cards/' + props.day + '.png'} />
+            <link rel="apple-touch-icon" sizes="180x180" href={'/favicons/' + props.day + '/apple-touch-icon.png'}/>
+            <link rel="icon" type="image/png" sizes="32x32" href={'/favicons/' + props.day + '/favicon-32x32.png'}/>
+            <link rel="icon" type="image/png" sizes="16x16" href={'/favicons/' + props.day + '/favicon-16x16.png'}/>
+            <link rel="manifest" href={'/favicons/' + props.day + '/site.webmanifest'}/>
             </>
             :
             <>
             <meta property="og:image" content={hostname + '/social-cards/monday.png'} />
-            <meta property="og:image:secure_url" content={hostname + '/social-cards/' + day + '.png'} />
+            <meta property="og:image:secure_url" content={hostname + '/social-cards/monday.png'} />
             <meta name="twitter:image" content={hostname + '/social-cards/monday.png'} />
             <link rel="apple-touch-icon" sizes="180x180" href={'/favicons/monday/apple-touch-icon.png'}/>
             <link rel="icon" type="image/png" sizes="32x32" href={'/favicons/monday/favicon-32x32.png'}/>
@@ -64,7 +64,7 @@ function HeadMetadata({title, description, day, type}) {
             </>
             }
             
-            {type !== undefined ? <meta property="og:type" content={type} /> : <meta property="og:type" content="website" />}
+            {props.type !== undefined ? <meta property="og:type" content={props.type} /> : <meta property="og:type" content="website" />}
             
             <meta property="og:locale" content="en_US" />
             <meta property="og:image:type" content="image/png" />
@@ -75,6 +75,9 @@ function HeadMetadata({title, description, day, type}) {
             <meta name="twitter:creator" content="@fewdaily"/>
             <meta name="twitter:site" content="@fewdaily"/>
             <meta name="twitter:card" content="summary_large_image" />
+
+            {props.children}
+            
         </Head>
     )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -11,10 +11,14 @@ class MyDocument extends Document {
       <Html lang="en" className="overflow-x-hidden w-full min-h-full">
         <Head>
           <meta name="theme-color" content="#282828"/>
+          <link rel="stylesheet"
+      href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/styles/a11y-dark.min.css" />
         </Head>
         <body className="relative overflow-x-hidden w-full min-h-full text-slate font-sans md:text-xl">
           <Main />
           <NextScript />
+          <script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/highlight.min.js"></script>
+          <script>hljs.initHighlightingOnLoad();</script>
         </body>
       </Html>
     )

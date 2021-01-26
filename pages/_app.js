99d26270937d3bd4cea885c3d6ce6dd20d3e6c2b
Morgan Spencer <morgan@spencercreative.co>
2021-01-26 10:53:57 -0500

    Update packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import App from 'next/app';
 import TagManager from 'react-gtm-module';
-import 'fontsource-mulish/500.css';
-import 'fontsource-mulish/700.css';
-import 'fontsource-mulish/900.css';
+import '@fontsource/mulish/500.css';
+import '@fontsource/mulish/700.css';
+import '@fontsource/mulish/900.css';
 import '../styles/index.scss';
 
 const tagManagerArgs = {
